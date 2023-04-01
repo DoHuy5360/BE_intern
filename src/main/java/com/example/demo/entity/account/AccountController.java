264a4f71944283e64bc0786868c726b016e50429
Dohuy5360 <loldh321@gmail.com>
2023-04-01 18:09:52 +0700

    Add CORS

--- src/main/java/com/example/demo/entity/account/AccountController.java
+++ src/main/java/com/example/demo/entity/account/AccountController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/account")
 // @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AccountController {
     @Autowired
     AccountService accountService;
--- src/main/java/com/example/demo/entity/workschedule/WorkScheduleController.java
+++ src/main/java/com/example/demo/entity/workschedule/WorkScheduleController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("/api/v1/workschedule")
+@CrossOrigin("*")
 public class WorkScheduleController {
     @Autowired
     private WorkScheduleService workScheduleService;
