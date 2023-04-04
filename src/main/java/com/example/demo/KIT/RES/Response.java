4a1862c705430453d45fda3c5ebb37b3be82a2c1
Dohuy5360 <loldh321@gmail.com>
2023-04-04 11:49:44 +0700

    Config response file: add amount attr

--- src/main/java/com/example/demo/KIT/RES/Response.java
+++ src/main/java/com/example/demo/KIT/RES/Response.java
@@ -2,11 +2,24 @@ package com.example.demo.KIT.RES;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Response {
 
     private HttpStatus status;
     private String message;
+    private int amount;
     private Object data;
+
+    public Response(HttpStatus status, String message, int amount, Object data) {
+        this.status = status;
+        this.message = message;
+        this.amount = amount;
+        this.data = data;
+    }
 
     public Response(HttpStatus status, String message, Object data) {
         this.status = status;
@@ -17,30 +30,6 @@ public class Response {
     public Response(HttpStatus status, String message) {
         this.status = status;
         this.message = message;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getdata() {
-        return data;
-    }
-
-    public void setdata(Object data) {
-        this.data = data;
     }
 
 }
