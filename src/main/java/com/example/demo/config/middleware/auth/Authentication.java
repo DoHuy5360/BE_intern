408be801135af87c7c40f50d8d49ad0e73c50ef0
Dohuy5360 <loldh321@gmail.com>
2023-04-12 00:19:28 +0700

    Increased Jwt token expired time up to 45 minutes

--- src/main/java/com/example/demo/config/middleware/auth/Authentication.java
+++ src/main/java/com/example/demo/config/middleware/auth/Authentication.java
@@ -81,7 +81,7 @@ public class Authentication implements HandlerInterceptor {
             printWriter.flush();
             return false;
         } else {
-            final int MINUTE = 15;
+            final int MINUTE = 45;
             final int SECOND = 60;
             final int MILLISECOND = 1000;
             ObjectMapper convertJson = new ObjectMapper();
