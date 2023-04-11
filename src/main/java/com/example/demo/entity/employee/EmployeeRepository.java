3d1ceeba98cb91a2f2aa4787892d216eb9874c67
Dohuy5360 <loldh321@gmail.com>
2023-04-11 14:56:36 +0700

    Remove redundant comment & import

--- src/main/java/com/example/demo/entity/employee/EmployeeRepository.java
+++ src/main/java/com/example/demo/entity/employee/EmployeeRepository.java
@@ -1,13 +1,7 @@
 package com.example.demo.entity.employee;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.example.demo.KIT.TRAY.EmployeeAccountHeadquarterTray;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, String> {
--- src/main/java/com/example/demo/testController.java
+++ src/main/java/com/example/demo/testController.java
@@ -1,30 +1,17 @@
 package com.example.demo;
 
-import java.text.MessageFormat;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.example.demo.KIT.JWT.JwtHandler;
 import com.example.demo.KIT.RES.Message;
 import com.example.demo.KIT.RES.Response;
 import com.example.demo.KIT.Util.DiscordLoger;
 
 @RestController
-// @CrossOrigin("*")
 public class testController {
 
     @GetMapping("/")
@@ -43,8 +30,6 @@ public class testController {
     }
 
     @PostMapping("/decode")
-    // public void decode(@RequestHeader("Authorization") String
-    // authorizationHeader) {
     public String decode() {
         return "Token still alive";
     }
